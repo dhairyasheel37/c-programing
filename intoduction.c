0796744befe816012391e7e5e2b7ad18aa29ba2a
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-04 22:21:45 +0530

    Add files via upload

---
+++ intoduction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the number");
+	scanf("%d %d",&a,&b);
+	if(a>b)
+	{
+	printf("The difference is %d",a-b);
+    }
+	else
+	{
+	printf("The sum is %d",a+b);
+	}
+}
+
