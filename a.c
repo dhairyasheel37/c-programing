a85afac634532cf686db22fd34549741c41f12fc
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-17 23:55:15 +0530

    Declare an array of four integers:

---
+++ a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // Declare an array of four integers:
+  int myNumbers[4];
+
+  // Add elements to it
+  myNumbers[0] = 25;
+  myNumbers[1] = 50;
+  myNumbers[2] = 75;
+  myNumbers[3] = 100;
+
+  printf("%d\n", myNumbers[0]);
+ 
+  return 0;
+}
