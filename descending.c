f1a052822d74057a1c14121062c307e64f3e2058
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-19 22:00:13 +0530

    Update descending.c

--- descending.c
+++ descending.c
@@ -15,32 +15,7 @@ void main()
 	{
 		for(j=i+1;j<5;j++)
 		{
-			if (a[i]>a[j])
-			{
-				t=a[i];
-				a[i]=a[j];
-				a[j]=t;
-			}
-		}
-	}
-		printf("#include<stdio.h>
-void main()
-{
-	
-	int a[5],i,j,t;
-	printf("enter the number \n");
-	for(i=0;i<5;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	printf("Array element are \n");
-	for(i=0;i<5;i++)
-	printf("a[%d]=%d\n",i,a[i]);
-	for(i=0;i<5;i++)
-	{
-		for(j=i+1;j<5;j++)
-		{
-			if (a[i]>a[j])
+			if (a[j]>a[i])
 			{
 				t=a[i];
 				a[i]=a[j];
@@ -54,10 +29,5 @@ void main()
 	
 		printf(" %d",a[i]);
 	}
-}order is ");
-	for(i=0;i<5;i++)
-	{
-	
-		printf(" %d",a[i]);
-	}
 }
+
