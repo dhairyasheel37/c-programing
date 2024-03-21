2c7d228bc246253463588ceee79ea8bf873c7ac9
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-21 23:26:23 +0530

    Create Add_Two_Numbers.c

---
+++ Add_Two_Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    // Loop for each test case
+    while (t--) {
+        int a, b;
+
+        scanf("%d %d", &a, &b);
+        printf("%d\n",a+b);
+
+        // Your code for each test case goes here 
+    }
+}
