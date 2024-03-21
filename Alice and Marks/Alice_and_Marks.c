65e0dc66d96b378e8ac4cf3c0178c589371f7e2b
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-21 23:57:04 +0530

    Create Alice_and_Marks.c

---
+++ Alice and Marks/Alice_and_Marks.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    // Your code goes here (if needed)
+  if (x >= 2 * y)
+    {
+    printf("YES");
+    }
+ else
+  { 
+      printf("No");
+  }
+}
