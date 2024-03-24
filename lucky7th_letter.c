931b04aab4e05f32fa117c78c389d7b47844e85d
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-24 23:25:52 +0530

    Add files via upload

---
+++ lucky7th_letter.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char S[10];
+    scanf("%s", &S);
+    char letter = S[6];
+    printf("%c\n",letter);
+    return 0;
+}
+
---
+++ taxes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int T;
+	scanf("%d",&T);
+	while(T--)
+	{
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    printf("%d",a-b);
+	}
+}
+
