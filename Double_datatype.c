74225bf892662a9efe16dfe19b741b97f78622c4
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-03-28 23:42:26 +0530

    Create Double_datatype.c

---
+++ Double_datatype.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+  double pi = 3.14;
+  double radius = 8.9;
+  double area = pi * radius * radius;
+
+  printf("The Area of the given Circle is %f", area);
+  
+}
