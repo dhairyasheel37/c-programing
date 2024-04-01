fe3c85e34fd56ec3f74b5ce21649a37aa6bbcfa4
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-04-01 23:14:03 +0530

    Create Reach on Time.c

---
+++ Reach on Time.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    
+	    if(a>=30)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+
+}
