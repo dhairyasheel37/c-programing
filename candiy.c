28fa4216885ad4624f6fd5824ac9b4262b17ee7e
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-04-07 21:48:51 +0530

    Create candiy.c

---
+++ candiy.c
@@ -0,0 +1,21 @@
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    if(a%3==0)
+	    {
+	    printf("YES\n");    
+	    }
+	    
+	    else
+	    {
+	     printf("NO\n");   
+	    }
+	     
+	}
+
+}
