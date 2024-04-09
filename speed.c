a648b904d929dce9fae022a3d201c74f8a7872a9
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-04-09 23:15:45 +0530

    Create speed.c

---
+++ speed.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int a,b;
+	    scanf("%d %d",&a,&b);
+	    if(a<b)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+
+}
+
