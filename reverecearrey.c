05a5bcbe4502058c1e49b0bf1702ba37f0699e15
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-04-16 16:51:30 +0530

    Add files via upload

---
+++ reverecearrey.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main() {
+    int size, i, temp;
+
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+
+    printf("Enter %d elements of the array:\n", size);
+    for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < size / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
+
+    printf("Reversed array: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
