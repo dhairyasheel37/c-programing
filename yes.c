58bc33b8377e603d570c397930efcd059fc34fb9
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-04-27 23:19:42 +0530

    Create yes.c

---
+++ yes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    while (T--) {
+        int X;
+        scanf("%d", &X);
+
+        if (X > 98)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
