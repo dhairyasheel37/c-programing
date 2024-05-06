659d170618fb89e294487b62a65ae8c1f9499e5f
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-05-06 23:11:51 +0530

    Create water.c

---
+++ #include <stdio.h>  int main(void) { 	/water.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	
+	while(t--)
+	{
+	    int a;
+	    scanf("%d",&a);
+	    
+	    printf("%d\n",a*2);
+	}
+}
