cbb24f708349dd977f5218fadfc9214f781c5ed4
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-05-07 21:25:41 +0530

    Create Population.c

---
+++ Population.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	
+	while(t--)
+	{
+	    int a,b,c;
+	    scanf("%d %d %d",&a,&b,&c);
+	     printf("%d\n", a-b+c);
+	    
+	}
+
+}
