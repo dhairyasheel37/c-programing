d7ecd411f840d03522dd6e3fd044d25fba550e17
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-07-08 23:23:52 +0530

    Update_Minimum_Coins.c

--- Minimum_Coins.c
+++ Minimum_Coins.c
@@ -12,5 +12,20 @@ int main(void) {
 	    coins=rupees%10;
 	    printf("%d\n",coins);
 	}
+/* There are only 
+2
+2 type of denominations in Chefland:
+
+Coins worth 
+1
+1 rupee each
+Notes worth 
+10
+10 rupees each
+Chef wants to pay his friend exactly 
+𝑋
+X rupees. What is the minimum number of coins Chef needs to pay exactly 
+𝑋
+X rupees?*/
 
 }
