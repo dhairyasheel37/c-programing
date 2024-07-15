67e2ed9e71c19d9ad7dfa78bbb9cdaf70ccde6f3
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-07-15 22:25:16 +0530

    Add files via upload

---
+++ c_program_practice_VVK/Celsius.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int Celsius;
+    float Fahrenheit;
+    printf("enter the celsius :");
+    scanf("%d",&Celsius);
+    Fahrenheit = 1.8*Celsius+32;
+    printf("Temperature in Fahrenheit %f",Fahrenheit);
+
+}
