5a52c62a3412cb1988bcb9cce35db62cc0439466
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-07-17 23:42:49 +0530

    Add files via upload

---
+++ 24_hypo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the number ");
+    scanf("%d%d",&a,&b);
+    a=a*a;
+    b=b*b;
+    c=sqrt(a+b);
+    printf(" Hy%d",c);
+}
