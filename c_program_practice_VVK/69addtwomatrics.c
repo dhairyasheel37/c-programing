e76b8f4e907ffd7eabaec3c800aaa39f517ad98d
Dhairyasheel Gaikwad <159804956+dhairyasheel37@users.noreply.github.com>
2024-07-30 23:20:00 +0530

    Add files via upload

---
+++ c_program_practice_VVK/69addtwomatrics.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],b[10][10],c[20][20],i,j,rows,column;
+    printf("Enter the row and column :");
+    scanf("%d %d",&rows,&column);
+    printf("enter matrix a is :\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("enter matrix b is :\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+
+    }
+
+
+    printf(" matrix a is :\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+       printf("\n");
+    }
+
+     printf(" matrix b is :\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            printf("%d ",b[i][j]);
+        }
+       printf("\n");
+    }
+
+
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+
+    printf("addition of two matrix is :\n");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<column;j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+       printf("\n");
+    }
+
+}
